2c9050f47ddcb8e262f428100477c8d7cef41e02
David Rodriguez <dsebastianrodriguez@ucundinamarca.edu.co>
2021-09-12 00:35:47 -0500

    Se elaboro la tabla con Angular Material y se hizo uso del PosgressBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatTableModule} from '@angular/material/table';
+import { TablaDeDatosComponent } from './pages/tabla-de-datos/tabla-de-datos.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaDeDatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatProgressBarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
